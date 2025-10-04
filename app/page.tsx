a58a201c3551565470aa1585909d5d5f9ea9bf0c
kurtamie <disarangkuti@gmail.com>
2025-10-04 17:58:55 +0700

    sec commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (  
     <div className="flex gap-12 items-center justify-center text-xl bg-[#f4f4f4] text-black">
-      landing
+      landing ye
     </div>
   );
 }
